24dc2794d84a582a729d020adef4011bb529033a
tibo <t.brayelecureuil@gmail.com>
2026-02-13 16:46:20 +0100

    c'est pour ton pays, c'est pour la magie

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
         print("Humidity:", dhtDevice.humidity)
         print("Temperature:", dhtDevice.temperature)
         print("================================")
-        sleep(2)
+        sleep(5)
 
 if __name__ == "__main__":
     main()
